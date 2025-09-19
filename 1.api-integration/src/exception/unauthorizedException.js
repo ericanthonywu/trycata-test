3d8e1d46b1454ccb49e9845409ade20222614eb6
ericanthony <ericanthony@ericanthony.com>
2025-09-20 02:09:44 +0700

    remove: delete auth middleware and unauthorized exception modules

--- 1.api-integration/src/exception/unauthorizedException.js
+++
@@ -1,7 +0,0 @@
-const STATUS_CODE = require("../constant/statusCodeConstant");
-
-class UnauthorizedException extends Error {
-  status = STATUS_CODE.UNAUTHORIZED;
-}
-
-module.exports = UnauthorizedException;
